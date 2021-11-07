bbb05331513e2d338a109fea11044557f9c8b2ba
Glenn Dixon Jr <steve@stevenbrownlee.com>
2021-11-07 15:16:13 -0600

    debugged conjunction function

--- practice.js
+++ practice.js
@@ -165,10 +165,18 @@
 // const myDay = dayPlanner(tuesdayChores, wednesdayChores, mondayChores, {firstName: "Glenn", lastName: "Dixon"}, currentDay[1])
 // console.log(myDay)
 
-const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
-let y = 1
+// const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// let y = 1
 
-for (let y = 1; y < cookies.length; y++) {
-    const currentCookie = cookies[y]
-    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// for (let y = 1; y < cookies.length; y++) {
+//     const currentCookie = cookies[y]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
+
+const conjunction = function (firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`
+    return conjoinedWord
 }
+
+console.log(conjunction("Master", "Card"))
+
